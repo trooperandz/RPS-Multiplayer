0e7341a29ee9f4fa98529bd1a0b29817a6b27443
Matt Holland <mtholland10@gmail.com>
2016-09-16 11:01:08 -0500

    Update db logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,6 +25,9 @@ var player1Losses = 0;
 var player2Wins   = 0;
 var player2Losses = 0;
 
+var player1Exists;
+var player2Exists;
+
 // Set globals for storing player names
 var player1Name = "";
 var player2Name = "";
@@ -32,14 +35,148 @@ var player2Name = "";
 // Set global for storing player's play number.  Used for restricting clicks on other player's move choice container
 var myPlayerNumber = 0;
 
-// Set global for storing player's name.  Used for showing player's name along with their message in the chat window. Stored locally on client.
-// Note: Not being used yet.  Haven't figured out what to do with this, & how to set name if player reloads their page...
+// Set global for storing player's name.  Used for showing player's name along with their message in the chat window. 
+// Stored as session var on client, so that if player reloads windown, their name is still saved and sill display in the chat window.
 var myPlayerName = "";
 
 // Set global class name for player-active styling instruction
 var activeClassName = "player-active";
 
+// Set global for chat message action, default == true (so that initial msg will appear).  Chat msg will only be appended to chat area if chatMsgEntered == true
+var chatMsgEntered = true;
+
+// Firebase db object, for defining methods etc for queries
+var dbObj = {
+
+	// Property referencing db 'chat' node
+	chatRef: db.ref('chat'),
+
+	// Property referencing db 'players' node
+	playersRef: db.ref('players'),
+
+	// Property referencing db 'turn' node
+	turnRef: db.ref('turn'),
+
+	// This works
+	getChat: function() {
+		this.chatRef.on('value', function(snapshot) {
+			if(snapshot.val() != null) {
+				$('#chat-textarea').append(snapshot.val() + "\n");
+			}
+			console.log("The chat value function ran! snapshot = " , snapshot.val());
+		});
+	},
+
+	// This works
+	getPlayers: function() {
+		this.playersRef.on('value', function(snapshot) {
+			// Check to see if players exist. If so, set player1 and player2 names
+			player1Exists = snapshot.child('1').exists();
+			player2Exists = snapshot.child('2').exists();
+
+			// If player 1 exists, set player1 name (it's okay to reset every time. Ensures that name stays set)
+			if(player1Exists) {
+				player1Data   = snapshot.child('1').val();
+				player1Name   = player1Data.name;
+				player1Wins   = player1Data.wins;
+				player1Losses = player1Data.losses;
+
+				// Update side content and main content player name
+				$('.player1-side-h').html(player1Name);
+				$('#player1Name').html(player1Name);
+
+				// Update player win/loss stats. Note: getBattleResults in snapshot sets globals for you
+				document.getElementById("player1-win-count").innerHTML = player1Wins;
+				document.getElementById("player1-loss-count").innerHTML = player1Losses;
+			}
+
+			// If player 2 exists, set player2 name
+			if(player2Exists) {
+				player2Data   = snapshot.child('2').val();
+				player2Name   = player2Data.name;
+				player2Wins   = player2Data.wins;
+				player2Losses = player2Data.losses;
+
+				// Update side content and main content player name
+				$('.player2-side-h').html(player2Name);
+				$('#player2Name').html(player2Name);
+
+				// Update player win/loss stats. Note: getBattleResults in snapshot sets globals for you
+				document.getElementById("player2-win-count").innerHTML = player2Wins;
+				document.getElementById("player2-loss-count").innerHTML = player2Losses;
+			}
+			console.log("turnCount inside of getPlayers(): " + turnCount);
+			// If turnCount == 1 (player 1's turn), add active class to content for styling
+			if(turnCount == 0 || turnCount == 1) {
+				// If both players' names have been entered, and player one hasn't moved yet, highlight player 1 content area and heading
+				if(player1Exists && player2Exists) {
+					var elementArray = [ $('#player1Name'), $('#player1') ];
+					game.addActiveClass(elementArray);
+				}
+
+			} else if (turnCount == 2) {
+				// If player one already moved, highlight player 2 content area and heading. First, remove class from player 1
+				var elementArray = [ $('#player1Name'), $('#player1') ];
+				game.removeActiveClass(elementArray);
+	
+				var elementArray = [ $('#player2Name'), $('#player2') ];
+				game.addActiveClass(elementArray);
+			
+			} else if (turnCount > 2) {
+				// Remove active class from player 2 content
+				var elementArray = [ $('#player2Name'), $('#player2')];
+				game.removeActiveClass(elementArray);
+
+				// Show move choices on both screens now that both players have chosen a move
+				var imgUrl1 = player1Data.imgUrl;
+				//console.log("Player 1 snapshot img test turnCount 3: " + imgUrl1);
+	
+				 var player1Choice = player1Data.choice;
+				// console.log("Player 1 snapshot choice test turnCount 3: " + choiceTest);
+	
+				// Replace choice content with player-selected move
+				game.showMoveSelected(imgUrl1, $('#player1'));
+	
+				var imgUrl2 = player2Data.imgUrl;
+				//console.log("Player 2 snapshot img test turnCount 3: " + imgUrl2);
+	
+				var player2Choice = player2Data.choice;
+				// console.log("Player 2 snapshot choice test turnCount 3: " + choiceTest);
+	
+				// Replace choice content with player-selected move
+				game.showMoveSelected(imgUrl2, $('#player2'));
+
+				// Reset turn node in db back to 1
+				dbObj.turnRef.set(1);
+				
+				// Determine who won, display middle feedback msg, update win/loss count, and then reset answer choice and middle feedback content to default
+				game.getBattleOutcome(player1Choice, player2Choice);
+			}
+
+			console.log("The players value function ran: snapshot = " , snapshot.val());
+			console.log("player1Exists: " + player1Exists + "\n player1Name: " + player1Name);
+			console.log("player2Exists: " + player2Exists + "\n player2Name: " + player2Name);
+		});
+	},
+
+	//
+	getTurn: function() {
+		this.turnRef.on('value', function(snapshot) {
+			turnCount = snapshot.val();
+			console.log("The turn value function ran: snapshot = " , snapshot.val());
+		});
+	}
+}
+
+// Activate event listener for added chat messages. Will only fire and append new chat message when the 'chat' child changes
+dbObj.getChat();
+dbObj.getTurn();
+dbObj.getPlayers();
+
+
+
 // Set values when items are added to db
+/*
 db.ref().on("value", function(snapshot) {
 	console.log("snapshot: " , snapshot.val());
 
@@ -122,9 +259,9 @@ db.ref().on("value", function(snapshot) {
 		document.getElementById("player2-win-count").innerHTML = player2Wins;
 		document.getElementById("player2-loss-count").innerHTML = player2Losses;
 	}
-
-	// Update chat window with most recent chat message
-	if (snapshot.child("chat").exists()) {
+	
+	// Update chat window with most recent chat message, only if child "chat" exists and chatMsgEntered == true (true state is set by msg click handler)
+	if (snapshot.child("chat").exists() && chatMsgEntered == true) {
 		$('#chat-textarea').append(snapshot.val().chat + "\n");
 	}
 
@@ -141,7 +278,7 @@ db.ref().on("value", function(snapshot) {
 // If any errors are experienced, log them to console. 
 }, function (errorObject) {
   	console.log("The read failed: " + errorObject.code);
-});
+});*/
 
 // Main game object for play functionality
 var game = {
@@ -210,7 +347,6 @@ var game = {
 		elementArray.forEach(function(element, index, arr) {
 			element.removeClass(activeClassName);
 		})
-		//htmlObj.removeClass(activeClassName);
 	},
 
 	/**
@@ -219,7 +355,7 @@ var game = {
 	 * @return N/A
 	 */
 	showMoveSelected: function(imgUrl,htmlObj) {
-		// Build choice selected html content. Add class "no-hover" so that the hover white bg is not initialized
+		// Build choice selected html content. Add class "no-hover-bg" so that the hover white bg is not initialized
 		var content =
 		'<div class="row">' +
         	'<div class="col-sm-12 col-md-12 col-lg-12" id="answer-reveal">' +
@@ -309,28 +445,45 @@ var game = {
 		} else if (gameResult) {
 			console.log("Player 1 won!");
 			// Player 1 won. Update player 1 win and player 2 loss count in db
-			db.ref().child("players").child("1").update({
+			// Note: when these 2 separate db instructions for updating wins and losses in db, if never executes the second loss update. Why?
+
+			/* This erases data. Doesn't work.  BUT it does set both values!
+			dbObj.playersRef.update({
+				1: {
+					wins: player1Wins
+				},
+
+				2: {
+					losses: player2Losses
+				}
+			});*/
+
+			dbObj.playersRef.child("1").update({
 				wins: player1Wins
-			})
-			db.ref().child("players").child("2").update({
+			});
+			
+			dbObj.playersRef.child("2").update({
 				losses: player2Losses
-			})
+			});
 
 			winMsg = "Player 1 won!";
 
 		} else if (!gameResult) {
 			console.log("Player 2 won!");
 			// Player 2 won
-			db.ref().child("players").child("2").update({
+			dbObj.playersRef.child("2").update({
 				wins: player2Wins
-			})
-			db.ref().child("players").child("1").update({
+			});
+
+			dbObj.playersRef.child("1").update({
 				losses: player1Losses
-			})
+			});
 
 			winMsg = "Player 2 won!";
-
 		}
+
+		// Reset turn node in db back to 1
+		//dbObj.turnRef.set(1);
 
 		// Play the battle decided noise
 		game.playGameNoise("battle-decided-sound");
@@ -360,7 +513,7 @@ var game = {
 		$('#round-feedback').text(msg);
 
 		// Set turnCount back to 1 so that player 1 can go (otherwise, will get an out-of-turn message)
-		turnCount = 1;
+		//turnCount = 1;
 		
 		// Play the next move alert noise
 		game.playGameNoise("next-move-sound");
@@ -433,8 +586,11 @@ $(document).ready(function() {
 			// Play the player joined noise
 			game.playGameNoise("player-join-noise");
 
+			// Set player1 name to name entered in Join Game input
 			player1Name = name;
 
+			// Set myPlayerName as session var, so that it will still exist on page reload.
+			localStorage.setItem("myPlayerName", name);
 			myPlayerName = name;
 
 			// Set player's number (unique to their computer).  Will dictate click rights
@@ -448,7 +604,8 @@ $(document).ready(function() {
 						name: name,
 						wins: 0
 					}
-				}
+				},
+				turn: 0
 			});
 		// Note: activate the below line if you want to disallow remote player from entering their opponent's name
 		//} else if (player1Name != "" && player2Name == "" && myPlayerNumber == 0) {
@@ -457,21 +614,26 @@ $(document).ready(function() {
 			// Play the player joined noise
 			game.playGameNoise("player-join-noise");
 
+			// Set player2 name to name entered in Join Game input
 			player2Name = name;
 
+			// Set myPlayerName as session var, so that it will still exist on page reload.
+			localStorage.setItem("myPlayerName", name);
 			myPlayerName = name;
 
 			// Set player's number (unique to their computer).  Will dictate click rights
 			myPlayerNumber = 2;
 
-			// Insert player info into db
-			db.ref().child("players").child("2").set({
+			// Insert player 2 info into db
+			dbObj.playersRef.child('2').set({
 				losses: 0,
 				name: name,
 				wins: 0
 			});
 
-			db.ref().child("turn").set(1);
+			// Update turn node to 1
+			dbObj.turnRef.set(1);
+			
 		} else {
 			game.showModal("There are already two active players! Somebody must leave first before adding a new player.");
 			return false;
@@ -497,6 +659,9 @@ $(document).ready(function() {
 			game.showModal("You can't go right now; It's not your turn!");
 			return false;
 		}
+
+		// Reset to false so that program will not append the most recent chat every time db action occurs
+		chatMsgEntered = false;
 		
 		// Get the move selected
 		var move = $(this).attr("id");
@@ -517,10 +682,10 @@ $(document).ready(function() {
 			game.showMoveSelected(imgUrl, $('#player1'));
 
 			// Set turnCount with updated value
-			db.ref().child("turn").set(turnCount);
+			dbObj.turnRef.set(turnCount);
 
 			// Add "choice" as child to players "1" object
-			db.ref().child("players").child("1").update({
+			dbObj.playersRef.child("1").update({
 				choice: move,
 				imgUrl: imgUrl
 			});
@@ -535,10 +700,10 @@ $(document).ready(function() {
 			game.showMoveSelected(imgUrl, $('#player2'));
 
 			// Set turnCount with updated value
-			db.ref().child("turn").set(turnCount);
+			dbObj.turnRef.set(turnCount);
 
 			// Add "choice" as child to players "2" object
-			db.ref().child("players").child("2").update({
+			dbObj.playersRef.child("2").update({
 				choice: move,
 				imgUrl: imgUrl
 			});
@@ -565,15 +730,27 @@ $(document).ready(function() {
 			return false;
 		}
 
-		// Add player's name to their text message, for display in chat window
+		// Add player's name to their text message, for display in chat window. If page was reloaded (erasing their local name), load session var
+		if (myPlayerName == "" && localStorage.getItem("myPlayerName") != "") {
+			myPlayerName = localStorage.getItem("myPlayerName");
+		}
+
 		text = myPlayerName + ": " + text;
 
 		// Clear out the chat text input after it is submitted
 		$('#chat-input').val("");
 
+		// Set chat state so that db will only update the chat area when a message has been entered (otherwise will append when player chooses move)
+		chatMsgEntered = true;
+
+		// Update the chat node with the chat input text
+		dbObj.chatRef.set(text);
+		/*
 		db.ref().update({
 			chat: text
-		});
+		});*/
+
+		//dbObj.updateChat();
 
 	});
 
@@ -610,6 +787,7 @@ $(document).ready(function() {
     // Allow user to enter their chat message via the "Enter" key
  	$('input#chat-input').keypress(function(e) {
         if (e.which == 13) {
+        	// Run the click handler for chat input action
             $("span.share-icon").click();
         }
     });
